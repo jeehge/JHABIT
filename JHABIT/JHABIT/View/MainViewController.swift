e95e53f6fe967d78dc94b7f5990f1e17b5557b22
JH <taerg89@gmail.com>
2022-03-26 13:18:07 +0900

    Setting Folder struct

---
+++ JHABIT/JHABIT/View/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  JHABIT
+//
+//  Created by JH on 2022/03/26.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}

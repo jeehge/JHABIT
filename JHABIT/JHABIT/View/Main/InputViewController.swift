2673dbf3948139c707c29441ebb1829d68d02fbe
JH <taerg89@gmail.com>
2022-04-02 01:42:56 +0900

    Setting Input Screen

--- JHABIT/JHABIT/View/Main/InputViewController.swift
+++ JHABIT/JHABIT/View/Main/InputViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 final class InputViewController: BaseViewController {
 	
 	// MARK: - Properties
-	@IBOutlet private weak var textFiled: UITextField!
+	@IBOutlet private weak var infoLabel: UILabel!
+	@IBOutlet private weak var inputTextFiled: UITextField!
+	@IBOutlet private weak var saveButton: UIButton!
 	
 	// MARK: - Life Cycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+		configView()
+	}
+	
+	// MARK: - Initialize
+	private func configView() {
+		infoLabel.text = "Github 아이디를 입력해주세요 🌱"
+		
+		saveButton.setTitle("저장", for: .normal)
+	}
+	
+	// MARK: - IBAction
+	@IBAction private func saveButtonTouchUpInside(_ sender: UIButton) {
+		Storage.githubID = inputTextFiled.text
+		
+		let mainVC: MainViewController = MainViewController.viewController(from: .main)
+		mainVC.modalPresentationStyle = .fullScreen
+		mainVC.modalTransitionStyle = .crossDissolve
+		present(mainVC, animated: true, completion: nil)
 	}
 }

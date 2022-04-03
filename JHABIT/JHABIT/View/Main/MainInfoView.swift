15e802db7f02a09a8948543d0730a0282d99d4d0
JH <taerg89@gmail.com>
2022-04-03 22:36:19 +0900

    add Files

---
+++ JHABIT/JHABIT/View/Main/MainInfoView.swift
@@ -0,0 +1,8 @@
+//
+//  MainInfoView.swift
+//  JHABIT
+//
+//  Created by JH on 2022/04/02.
+//
+
+import Foundation
---
+++ JHABIT/JHABIT/View/Setting/SettingViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SettingViewController.swift
+//  JHABIT
+//
+//  Created by JH on 2022/04/02.
+//
+
+import Foundation
